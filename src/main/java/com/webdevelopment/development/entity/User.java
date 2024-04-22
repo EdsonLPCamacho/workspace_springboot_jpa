28ca504e7b1178dae2833f2a58192406befeb15c
Edson Camacho <edsonlpcamacho@gmail.com>
2024-04-22 10:33:48 -0300

    OrdeItem implemented

--- src/main/java/com/webdevelopment/development/entity/User.java
+++ src/main/java/com/webdevelopment/development/entity/User.java
@@ -1,12 +1,15 @@
 package com.webdevelopment.development.entity;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -21,6 +24,9 @@ public class User implements Serializable {
 	private String name;
 	private String email;
 	private String password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<OrderItem> orders = new ArrayList<>();
 
 //Constructors
 	public User() {
@@ -66,6 +72,10 @@ public class User implements Serializable {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
+	public List<OrderItem> getOrders() {
+		return orders;
+	}
 
     //hashCode & equals
 	@Override
@@ -84,4 +94,6 @@ public class User implements Serializable {
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
+
+
 }
